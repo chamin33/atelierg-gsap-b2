3295214517f92ee83cf213b92a02acb620b96cb5
unknown <carlotta.genet@gmail.com>
2025-02-09 19:52:34 +0100

    afinir

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 gsap.registerPlugin(ScrollTrigger,Observer,ScrollToPlugin,Draggable,MotionPathPlugin);
 
 //Gsap demo animation - window 1 rotation 
-
+/*
 gsap.to(".window1", {
     scrollTrigger:{
         trigger: "#section1", 
@@ -34,26 +34,16 @@ gsap.from(".window2",{
     
     x:'-100vw',
     duration: 1,
-})
+})*/
 
-gsap.to(".window3",{
-    duration: 5,
-    motionPath: {
-        path: "M67 1C47.3333 3 9.40001 18.4 15 64C16.6286 77.2617 22.3171 86.1388 30 91.6513M30 91.6513C55.3388 109.832 102.371 91.4161 97 73C90 49 27 68.3027 30 91.6513ZM30 91.6513C20 119.434 13.4 174.8 67 174C134 173 130.061 260 56 260C-14 260 -3 335 15 359C30 379 154 400 183 298",
 
-    },
-    /*onComplete: ()=> {
-        document.getElementById('titleSec3').style.color="red";
-    },*/
-
-})
 
 // GSAP animation - window 4 timeline
-let timeWindow4 = gsap.timeline();
+/*let timeWindow4 = gsap.timeline();
 timeWindow4.to('.window4', {x:100,})
 .to('.window4', {y:100,})
 .to('.window4', {x:-100,})
-.to('.window4', {y:-100,})
+.to('.window4', {y:-100,})*/
 
 
 
